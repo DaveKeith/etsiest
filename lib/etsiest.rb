514649d2ba89f33cf0953a80b6687cfc286003b2
DaveKeith <d.keith78@yahoo.com>
2016-04-05 19:56:06 -0400

    the program can now take in images from etsy along with the captions below the image

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,26 @@
+require "pry"
+require "json"
 require "etsiest/version"
+require "sinatra/base"
+require "uri"
+
+require "etsy"
+
+Etsy.api_key = ENV["ETSY_KEY"]
+# <%= results %>
 
 module Etsiest
-  # Your code goes here...
+  class App < Sinatra::Application
+    get '/search' do
+      query = params["q"]
+      #maybe have if for missing "q"?
+      # response = Etsy.search_thing(response)
+      response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'],
+          :keywords => query)
+
+      binding.pry
+      erb :index, locals: {results: response.result}
+    end
+    run! if app_file == $0
+  end
 end
