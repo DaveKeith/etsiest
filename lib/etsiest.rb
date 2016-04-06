190ea55a4f1a2b531e0ced46f9ee8eb47dfd1184
DaveKeith <d.keith78@yahoo.com>
2016-04-05 20:00:21 -0400

    got rid of binding.pry and a few other comments

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -7,18 +7,14 @@ require "uri"
 require "etsy"
 
 Etsy.api_key = ENV["ETSY_KEY"]
-# <%= results %>
 
 module Etsiest
   class App < Sinatra::Application
     get '/search' do
       query = params["q"]
-      #maybe have if for missing "q"?
-      # response = Etsy.search_thing(response)
       response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'],
           :keywords => query)
 
-      binding.pry
       erb :index, locals: {results: response.result}
     end
     run! if app_file == $0
